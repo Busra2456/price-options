9c7a549130fb20f139f5ece0d00b3fc758721e74
Busra2456 <you@example.com>
2024-09-25 12:26:38 +0600

    features grow

--- src/components/Feature/Feature.jsx
+++ src/components/Feature/Feature.jsx
@@ -1,9 +1,9 @@
-
+import {AiFillCheckCircle} from "react-icons/ai";
 import PropTypes from "prop-types";
 const Feature = ({feature}) => {
       return (
             <div>
-                <p> {feature} </p>  
+                <p className="flex items-center" >< AiFillCheckCircle className="text-green-700 m-2" ></AiFillCheckCircle>{feature} </p>  
             </div>
       );
 };
